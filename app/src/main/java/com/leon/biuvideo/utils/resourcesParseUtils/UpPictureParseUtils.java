82871fd65b34f55d577329a71f60a71ded60927b
leon <leon2656@163.com>
2020-10-22 09:51:27 +0800

    基本信息、视频、音频和相簿的解析都已完成，相簿的解析还有待完善，这IDE有毒。。。

--- app/src/main/java/com/leon/biuvideo/utils/resourcesParseUtils/UpPictureParseUtils.java
+++ app/src/main/java/com/leon/biuvideo/utils/resourcesParseUtils/UpPictureParseUtils.java
@@ -87,7 +87,6 @@ public class UpPictureParseUtils {
             return upPictures;
         }
 
-
         Log.e(LogTip.red, "doc_list接口数据获取失败", new NullPointerException("doc_list接口数据获取失败"));
         return null;
     }
