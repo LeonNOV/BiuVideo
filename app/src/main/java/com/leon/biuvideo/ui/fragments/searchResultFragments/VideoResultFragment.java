ba27321fef675da0e9fe8d46d12a6fe2f871ca49
Leon <leon2656@163.com>
2020-11-23 09:37:24 +0800

    在`VideoResultFragment`中添加了加载更多功能

--- app/src/main/java/com/leon/biuvideo/ui/fragments/searchResultFragments/VideoResultFragment.java
+++ app/src/main/java/com/leon/biuvideo/ui/fragments/searchResultFragments/VideoResultFragment.java
@@ -1,9 +1,13 @@
 package com.leon.biuvideo.ui.fragments.searchResultFragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +18,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.leon.biuvideo.R;
 import com.leon.biuvideo.adapters.UserFragmentAdapters.UserVideoAdapter;
 import com.leon.biuvideo.beans.upMasterBean.UpVideo;
+import com.leon.biuvideo.utils.Fuck;
 import com.leon.biuvideo.utils.OrderType;
 import com.leon.biuvideo.utils.searchParsers.VideoParser;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 
 import java.util.List;
 
@@ -25,9 +32,18 @@ public class VideoResultFragment extends Fragment {
     private RecyclerView search_result_recyclerView;
     private String keywordCoded;
     private int count;
+    private int currentCount;
 
     private VideoParser videoParser;
     private List<UpVideo> videos;
+    private Context context;
+
+    private boolean dataState = true;
+    private int pageNum = 1;
+
+    public VideoResultFragment(String keywordCoded) {
+        this.keywordCoded = keywordCoded;
+    }
 
     @Nullable
     @Override
@@ -42,6 +58,8 @@ public class VideoResultFragment extends Fragment {
 
     private void initView() {
         View view = getView();
+        context = getContext();
+
         search_result_smartRefresh = view.findViewById(R.id.search_result_smartRefresh);
         search_result_recyclerView = view.findViewById(R.id.search_result_recyclerView);
 
@@ -54,12 +72,58 @@ public class VideoResultFragment extends Fragment {
         count = VideoParser.getSearchVideoCount(keywordCoded);
 
         videoParser = new VideoParser();
-        videos = videoParser.videoParse(keywordCoded, 1, OrderType.DEFAULT);
+        videos = videoParser.videoParse(keywordCoded, pageNum, OrderType.DEFAULT);
 
         UserVideoAdapter userVideoAdapter = new UserVideoAdapter(videos, getContext());
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
 
         search_result_recyclerView.setLayoutManager(linearLayoutManager);
         search_result_recyclerView.setAdapter(userVideoAdapter);
+
+        //添加加载更多监听事件
+        search_result_smartRefresh.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshLayout) {
+
+                if (dataState) {
+                    pageNum++;
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            //获取新数据
+                            List<UpVideo> addOns = getVideos(pageNum);
+
+                            Log.d(Fuck.blue, "成功获取了第" + pageNum + "页的" + addOns.size() + "条数据");
+
+                            //添加新数据
+                            userVideoAdapter.refresh(addOns);
+                        }
+                    }, 1000);
+                } else {
+                    //关闭上滑刷新
+                    search_result_smartRefresh.setEnabled(false);
+
+                    Toast.makeText(context, "只有这么多数据了~~~", Toast.LENGTH_SHORT).show();
+                }
+
+                //结束加载更多动画
+                search_result_smartRefresh.finishLoadMore();
+            }
+        });
+    }
+
+    public List<UpVideo> getVideos(int pageNum) {
+        List<UpVideo> temp = videoParser.videoParse(keywordCoded, pageNum, OrderType.DEFAULT);
+
+        //记录获取的总数
+        currentCount += temp.size();
+
+        //判断是否已获取完所有的数据
+        if (temp.size() < 30 || currentCount == count) {
+            dataState = false;
+        }
+
+        return videos;
     }
 }
