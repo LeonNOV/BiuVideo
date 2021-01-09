82bcc33851da0db16171cbc21f3f1b0b12e7b9d6
leon <leon2656@163.com>
2021-01-09 22:40:57 +0800

    加入了`BangumiActivity`

---
+++ app/src/main/java/com/leon/biuvideo/ui/fragments/bangumiFragments/BangumiDetailFragment.java
@@ -0,0 +1,22 @@
+package com.leon.biuvideo.ui.fragments.bangumiFragments;
+
+import com.leon.biuvideo.R;
+import com.leon.biuvideo.ui.fragments.baseFragment.BaseFragment;
+import com.leon.biuvideo.ui.fragments.baseFragment.BindingUtils;
+
+public class BangumiDetailFragment extends BaseFragment {
+    @Override
+    public int setLayout() {
+        return R.layout.fragment_bangumi_detail;
+    }
+
+    @Override
+    public void initView(BindingUtils bindingUtils) {
+
+    }
+
+    @Override
+    public void initValues() {
+
+    }
+}
---
+++ app/src/main/java/com/leon/biuvideo/ui/fragments/bangumiFragments/BangumiInfoFragment.java
@@ -0,0 +1,21 @@
+package com.leon.biuvideo.ui.fragments.bangumiFragments;
+
+import com.leon.biuvideo.R;
+import com.leon.biuvideo.ui.fragments.baseFragment.BaseFragment;
+import com.leon.biuvideo.ui.fragments.baseFragment.BindingUtils;
+
+public class BangumiInfoFragment extends BaseFragment {
+    @Override
+    public int setLayout() {
+        return R.layout.fragment_bangumi_info;
+    }
+
+    @Override
+    public void initView(BindingUtils bindingUtils) {
+    }
+
+    @Override
+    public void initValues() {
+
+    }
+}
